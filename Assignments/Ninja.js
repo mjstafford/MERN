2ab4453357ed0b5ce16f12b5af858809794f8598
Michael <67665934+mjstafford@users.noreply.github.com>
2022-08-29 11:23:25 -0700

    ninja practice complete

---
+++ Assignments/Ninja.js
@@ -0,0 +1,24 @@
+class Ninja {
+    constructor(name, health, speed = 3, strength = 3) {
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
+    }
+
+    sayName() {
+        console.log(this.name)
+    }
+
+    showStats() {
+        console.log(`Name: ${this.name}\nStrength: ${this.strength}\nSpeed: ${this.speed}\nHealth: ${this.health}\n`)
+    }
+
+    drinkSake() {
+        this.health += 10;
+    }
+}
+
+let ninja1 = new Ninja("george", 10, 5)
+
+ninja1.showStats()
