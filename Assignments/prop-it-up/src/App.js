f65250b6b04dc0e2139782eccccfe9049e51b712
Michael <67665934+mjstafford@users.noreply.github.com>
2022-08-31 11:22:39 -0700

    putting it together update

--- Assignments/prop-it-up/src/App.js
+++ Assignments/prop-it-up/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>test</h1>
       <PersonCard firstName="Michael" lastName="stafford" age={99} hairColor="brown" />
       <PersonCard firstName="Sara" lastName="white" age={97} hairColor="brown" />
       <PersonCard firstName="Butternut" lastName="White" age={9} hairColor="white/black" />
--- Assignments/prop-it-up/src/components/PersonCard.js
+++ Assignments/prop-it-up/src/components/PersonCard.js
@@ -1,17 +1,38 @@
 import React, { Component } from "react"
 
 class PersonCard extends Component {
+    //add a constructor to store the state
+    constructor(props) {
+        super(props)
+
+        //props will always be the same (forever!)
+        //BUT if you want something from props to change then put it into state
+        //AND reference the property in state instead of property in props
+        this.state = {
+            age: this.props.age
+        }
+    }
+
+    //add a function to change the state of age
+    birthday = () => {
+        this.setState({ age: this.state.age + 1 })
+        // document.querySelector(`#age-${this.props.firstName}`).textContent = `Age: ${this.state.age}`
+    }
+
+    //render the code
     render() {
-        //destructure 
-        const { firstName, lastName, age, hairColor } = this.props
+        //destructure in the render
+        const { firstName, lastName, hairColor } = this.props
+        const { age } = this.state      //age must come from state. If it was coming from props it would never change
 
         //return jsx
         return (
             <div>
                 <h1> {lastName}, {firstName}</h1>
-                <p>Age: {age}</p>
+                <p id={"age-" + firstName} >Age: {age}</p>
                 <p>Hair Color: {hairColor}</p>
-            </div>
+                <input type="button" value="Happy Birthday!" onClick={this.birthday} />
+            </div >
         )
     }
 }
